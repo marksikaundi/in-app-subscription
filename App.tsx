0adf87a0b3f732ae55564d5375eded2677eb87ad
marksikaundi <emmanuelmark117@gmail.com>
2023-02-05 17:52:54 +0200

    small config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
